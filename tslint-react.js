ff4da7f8b5e6af975b35f6c72a735031fb3c815b
Andréas HANSS <andreas.hanss67@gmail.com>
2019-12-29 12:56:05 +0100

    Config adjustment

--- tslint-react.js
+++ tslint-react.js
@@ -2,9 +2,6 @@ module.exports = {
   extends: ["tslint-react", "tslint-react-hooks", "./tslint-common.js"],
   rules: {
     "react-hooks-nesting": true, // Nesting hooks checker
-    "jsx-no-lambda": {
-      // Just warn because this is not critical everywhere.
-      severity: "warning"
-    }
+    "jsx-no-lambda": false // Nothing bad with lambdas, fix it on a case-by-case basis
   }
 };
